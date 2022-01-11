e7c287388b9185c9bb6cf23575cc4b3da331309d
arjantijms <arjan.tijms@gmail.com>
2022-01-11 18:29:52 +0100

    Correctly handle Servlet spec 13.8.4 Item 3
    Signed-off-by: arjantijms <arjan.tijms@gmail.com>

--- impl/src/main/java/org/glassfish/exousia/constraints/transformer/ElementsToConstraintsTransformer.java
+++ impl/src/main/java/org/glassfish/exousia/constraints/transformer/ElementsToConstraintsTransformer.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2021 Contributors to Eclipse Foundation.
  * Copyright (c) 2019 OmniFaces. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -30,13 +31,14 @@ import org.glassfish.exousia.constraints.WebResourceCollection;
 
 import jakarta.servlet.HttpMethodConstraintElement;
 import jakarta.servlet.ServletSecurityElement;
+import jakarta.servlet.annotation.HttpConstraint;
 import jakarta.servlet.annotation.HttpMethodConstraint;
 import jakarta.servlet.annotation.ServletSecurity;
 import jakarta.servlet.annotation.ServletSecurity.EmptyRoleSemantic;
 import jakarta.servlet.annotation.ServletSecurity.TransportGuarantee;
 
 /**
- * 
+ *
  * @author Arjan Tijms
  */
 public class ElementsToConstraintsTransformer {
@@ -48,36 +50,36 @@ public class ElementsToConstraintsTransformer {
 
         List<SecurityConstraint> constraints = new ArrayList<>();
         Set<String> httpMethodOmissions = new HashSet<>();
-        
+
         // Handle the constraints that hold for specific HTTP methods
 
         for (HttpMethodConstraintElement httpMethodConstraint : servletSecurityElement.getHttpMethodConstraints()) {
             constraints.add(createSecurityConstraint(
-                urlPatterns, 
+                urlPatterns,
                 httpMethodConstraint.getRolesAllowed(),
-                httpMethodConstraint.getEmptyRoleSemantic(), 
-                httpMethodConstraint.getTransportGuarantee(), 
+                httpMethodConstraint.getEmptyRoleSemantic(),
+                httpMethodConstraint.getTransportGuarantee(),
                 httpMethodConstraint.getMethodName(),
                 emptySet()));
 
-            // Methods handled by the httpMethodConstraint are not handled by special "all methods" constraint 
+            // Methods handled by the httpMethodConstraint are not handled by special "all methods" constraint
             httpMethodOmissions.add(httpMethodConstraint.getMethodName());
         }
-        
+
         // Handle the special constraint that holds for all HTTP methods, accept those handled by the method
         // specific constraints
-        
+
         constraints.add(createSecurityConstraint(
-                urlPatterns, 
+                urlPatterns,
                 servletSecurityElement.getRolesAllowed(),
-                servletSecurityElement.getEmptyRoleSemantic(), 
-                servletSecurityElement.getTransportGuarantee(), 
+                servletSecurityElement.getEmptyRoleSemantic(),
+                servletSecurityElement.getTransportGuarantee(),
                 null,
                 httpMethodOmissions));
-        
+
         return constraints;
     }
-    
+
     public static List<SecurityConstraint> createConstraints(Set<String> urlPatterns, ServletSecurity servletSecurity) {
         if (urlPatterns.isEmpty()) {
             return emptyList();
@@ -85,67 +87,85 @@ public class ElementsToConstraintsTransformer {
 
         List<SecurityConstraint> constraints = new ArrayList<>();
         Set<String> httpMethodOmissions = new HashSet<>();
-        
+
         // Handle the constraints that hold for specific HTTP methods
 
         for (HttpMethodConstraint httpMethodConstraint : servletSecurity.httpMethodConstraints()) {
             constraints.add(createSecurityConstraint(
-                urlPatterns, 
+                urlPatterns,
                 httpMethodConstraint.rolesAllowed(),
-                httpMethodConstraint.emptyRoleSemantic(), 
-                httpMethodConstraint.transportGuarantee(), 
+                httpMethodConstraint.emptyRoleSemantic(),
+                httpMethodConstraint.transportGuarantee(),
                 httpMethodConstraint.value(),
                 emptySet()));
 
-            // Methods handled by the httpMethodConstraint are not handled by special "all methods" constraint 
+            // Methods handled by the httpMethodConstraint are not handled by special "all methods" constraint
             httpMethodOmissions.add(httpMethodConstraint.value());
         }
-        
+
         // Handle the special constraint that holds for all HTTP methods, accept those handled by the method
-        // specific constraints
-        
+        // specific constraints.
+
+        // 13.8.4 Item 3:
+        // "A @ServletSecurity annotation includes an @HttpConstraint that returns all default values and it also includes
+        // "at least one @HttpMethodConstraint that returns other than all default values. All HTTP methods other than those
+        // named in an @HTTPMethodConstraint are uncovered by the annotation."
+
+        if ((isDefault(servletSecurity.value()) && servletSecurity.httpMethodConstraints().length > 0)) {
+            // The specified "special move" (combo) was executed, meaning we have to skip creating the special constraint.
+            return constraints;
+        }
+
         constraints.add(createSecurityConstraint(
-                urlPatterns, 
+                urlPatterns,
                 servletSecurity.value().rolesAllowed(),
-                servletSecurity.value().value(), 
-                servletSecurity.value().transportGuarantee(), 
+                servletSecurity.value().value(),
+                servletSecurity.value().transportGuarantee(),
                 null,
                 httpMethodOmissions));
-        
+
         return constraints;
+    }
+
+    private static boolean isDefault(HttpConstraint httpConstraint) {
+        return
+            httpConstraint.value().equals(EmptyRoleSemantic.PERMIT) &&
+            httpConstraint.transportGuarantee().equals(TransportGuarantee.NONE) &&
+            httpConstraint.rolesAllowed().length == 0;
     }
 
     private static SecurityConstraint createSecurityConstraint(Set<String> urlPatterns, String[] rolesAllowed, EmptyRoleSemantic emptyRoleSemantic, TransportGuarantee transportGuarantee, String httpMethod, Set<String> httpMethodOmissions) {
         return new SecurityConstraint(asList(
             new WebResourceCollection(
-                urlPatterns, createHttpMethods(httpMethod), httpMethodOmissions)), 
+                urlPatterns, createHttpMethods(httpMethod), httpMethodOmissions)),
             createRolesAllowed(
-                rolesAllowed, emptyRoleSemantic), 
+                rolesAllowed, emptyRoleSemantic),
             transportGuarantee);
     }
-    
+
     private static Set<String> createRolesAllowed(String[] rolesAllowed, EmptyRoleSemantic emptyRoleSemantic) {
         if (rolesAllowed != null && rolesAllowed.length > 0) {
             if (emptyRoleSemantic == DENY) {
                 throw new IllegalArgumentException("Cannot use DENY with non-empty rolesAllowed");
             }
-            
+
             return new HashSet<>(asList(rolesAllowed));
         }
-        
+
         if (emptyRoleSemantic == DENY) {
+            // Empty role set means DENY
             return emptySet();
         }
-        
+
         // Null means PERMIT
         return null;
     }
-    
+
     private static Set<String> createHttpMethods(String httpMethod) {
         if (httpMethod == null) {
             return emptySet();
         }
-        
+
         return new HashSet<>(asList(httpMethod));
     }
 
